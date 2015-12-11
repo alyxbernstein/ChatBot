c79a6ed389e25b166513222e1855831951acade1
Alyx Bernstein <alyxbernstein@gmail.com>
2015-12-11 10:31:55 -0500

    version 0.1.0

--- Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
+++ Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
@@ -1,13 +1,13 @@
 package com.example.adam_000.chatbot;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-
+        final EditText input = (EditText)findViewById(R.id.input);
+        Button enter = (Button)findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                TextView userIn = (TextView)findViewById(R.id.userIn);
+                String a = input.getText().toString();
+                String b = "User: ";
+                String c = a + b;
+                userIn.setText(c);
+                c.toLowerCase();
+                if (c.contains("hi") || c.contains("hello") || c.contains("hey")){
+                    TextView botOut = (TextView)findViewById(R.id.botOut);
+                    String d = "ShopBot: Hello, how are you?";
+                    botOut.setText(d);
+                }
+            }
+        });
     }
 
     @Override
