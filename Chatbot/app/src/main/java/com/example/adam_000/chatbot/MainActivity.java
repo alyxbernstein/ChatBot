7697372369c32085e03dfaceecb8b2c92f32bb20
Alyx Bernstein <alyxbernstein@gmail.com>
2015-12-16 14:01:14 -0500

    added clothing and product class

--- Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
+++ Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
@@ -28,6 +28,7 @@ TODO: responses
 public class MainActivity extends AppCompatActivity {
     private static final String CUSTOM_TAB_PACKAGE_NAME = "com.android.chrome";
     int count = 0;
+    int buy = 1;
     TextView userIn;
     TextView botOut;
     CustomTabsServiceConnection mCustomTabsServiceConnection;
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
     String site;
     ArrayList<String> userInput = new ArrayList<String>();
     ArrayList<String> botOutput = new ArrayList<String>();
+    ArrayList<String> keywords = new ArrayList<String>();
 
     public String prepURL(ArrayList<String> search){
         String temp = "https://google.com/search?espv=2&q=site%3A" + site;
@@ -64,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void chat(String in, int i, EditText e){
         String b = "User: ";
-        String c = in + b;
+        String c = b + in;
 
         switch (i){
             case 0:
@@ -130,52 +132,81 @@ public class MainActivity extends AppCompatActivity {
                 c.toLowerCase();
                 userInput.add(c);
                 if (c.contains("shirt")){
-
+                    count = 4;
+                    botOut = (TextView)findViewById(R.id.botOut3);
+                    String d = "What color?";
+                    botOut.setText(d);
+                    botOutput.add(d);
+                    keywords.add("shirt");
+                    break;
                 } else if (c.contains("pants")){
+                    count = 4;
+                    botOut = (TextView)findViewById(R.id.botOut3);
+                    String d = "What color?";
+                    botOut.setText(d);
+                    botOutput.add(d);
+                    keywords.add("pants");
+                    break;
 
                 } else if (c.contains("phone")){
                     //rant about how horrible the iPhone is, tell person to buy n5x or n6p
-                    count = 3;
-                    String d = "I'm going to assume you want an iPhone. I, however, was programmed in Android. Therefore, I mus compel you to purchase a Nexus Device. Would you like a nexus 6p or 5x?";
+                    botOut = (TextView)findViewById(R.id.botOut3);
+                    String d = "I'm going to assume you want an iPhone. I, however, was programmed in Android. Therefore, I must compel you to purchase a Nexus Device. Would you like a nexus 6p or 5x?";
                     botOut.setText(d);
                     botOutput.add(d);
-                    break;
+                    userIn = (TextView)findViewById(R.id.userIn4);
+                    userIn.setText(c);
+                    e.setText("");
+                    c.toLowerCase();
+                    userInput.add(c);
+                    if (c.contains("6p")){
+                        String newURL = "https://store.google.com/product/nexus_6p";
+                        goToWebpage(newURL);
+                        break;
+                    } else if (c.contains("5x")){
+                        String newURL = "https://store.google.com/product/nexus_5x";
+                        goToWebpage(newURL);
+                        break;
+                    } else {
+                        botOut = (TextView)findViewById(R.id.botOut4);
+                        botOut.setText(d);
+                        break;
+                    }
                 } else if (c.contains("laptop")){
                     //rant about how horrible the iPhone is, tell person to buy hp spectre x360
                     site = "bestbuy.com";
-                } else if (c.contains("console")){
-
-                } else if (c.contains("watch")){
-                    //rant about how horrible apple watch is, tell person to buy moto 360
                 } else {
                     String d = "I'm sorry, I don't understand";
                     botOut = (TextView)findViewById(R.id.botOut3);
                     botOut.setText(d);
                     break;
                 }
-            case 3:
-                userIn = (TextView)findViewById(R.id.userIn4);
+
+            case 4:
+                userIn = (TextView)findViewById(R.id.userIn5);
                 userIn.setText(c);
                 e.setText("");
                 c.toLowerCase();
                 userInput.add(c);
-                if (c.contains("6p")){
-                    String newURL = "https://store.google.com/product/nexus_6p";
-                    goToWebpage(newURL);
-                    break;
-                } else if (c.contains("5x")){
-                    String newURL = "https://store.google.com/product/nexus_5x";
-                    goToWebpage(newURL);
-                    break;
-                } else {
-                    String d = "I'm sorry, I don't understand";
-                    botOut = (TextView)findViewById(R.id.botOut4);
-                    botOut.setText(d);
-                    break;
+                String[] colors = new String[]{"red", "blue", "green", "white", "yellow", "black", "orange", "pink", "brown"};
+                for (String color : colors){
+                    if (c.contains(color)){
+                        keywords.add(0, color);
+                        switch (buy){
+                            case 1:
+
+                        }
+                    } else {
+                        String d = "I'm sorry, I don't understand";
+                        botOut = (TextView)findViewById(R.id.botOut4);
+                        botOut.setText(d);
+                        break;
+                    }
+                    }
                 }
         }
 
-    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
