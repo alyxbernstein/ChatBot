66051c8a737717fae185ce4a37607001381322f3
alyxbernstein <alyxbernstein@gmail.com>
2016-01-05 23:27:35 -0500

    final commit

--- Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
+++ Chatbot/app/src/main/java/com/example/adam_000/chatbot/MainActivity.java
@@ -15,6 +15,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -39,14 +41,6 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<String> botOutput = new ArrayList<String>();
     private ArrayList<String> keywords = new ArrayList<String>();
 
-    //returns URL
-    private String prepURL(ArrayList<String> search){
-        String URL = "https://google.com/search?espv=2&q=site%3A" + site;
-        for (String keyword : search){
-            URL += "+" + keyword;
-        }
-        return URL;
-    }
 
     private void goToWebpage(String URL){
         //go to webpage -
@@ -62,162 +56,12 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void buyPhone(int i, Smartphone phone){
-        switch (i){
-            case 1:
-                botOut = (TextView)findViewById(R.id.botOut3);
-                String d = "Would you like an unlocked or locked phone?";
-                botOut.setText(d);
-                i++;
-                buyPhone(i, phone);
-            case 2:
-                String in = input.getText().toString();
-                String b = "User: ";
-                String userInputText = b + in;
-                userIn = (TextView)findViewById(R.id.userIn4);
-                userIn.setText(userInputText);
-                input.setText("");
-                userInputText = userInputText.toLowerCase();
-                userInput.add(userInputText);
-                if (userInputText.contains("unlocked")){
-                    phone.setUnlocked(true);
-                    i++;
-                    buyPhone(i, phone);
-                } else if (userInputText.contains("carrier")){
-                    phone.setUnlocked(false);
-                    i++;
-                    buyPhone(i, phone);
-                } else {
-                    String done = "I'm sorry, I don't understand";
-                    botOut = (TextView)findViewById(R.id.botOut4);
-                    botOut.setText(done);
-                    buyPhone(i, phone);
-                }
-            case 3:
-                botOut = (TextView)findViewById(R.id.botOut4);
-                d = "What brand would you like?";
-                botOut.setText(d);
-                in = input.getText().toString();
-                b = "User: ";
-                userInputText = b + in;
-                userIn = (TextView)findViewById(R.id.userIn5);
-                userIn.setText(userInputText);
-                input.setText("");
-                userInputText = userInputText.toLowerCase();
-                userInput.add(userInputText);
-                if (userInputText.contains("apple")){
-                    botOut = (TextView)findViewById(R.id.botOut5);
-                    d = "Which model do you want, 5s, 6 or 6s?";
-                    botOut.setText(d);
-                    in = input.getText().toString();
-                    b = "User: ";
-                    userInputText = b + in;
-                    userIn = (TextView)findViewById(R.id.userIn1);
-                    userIn.setText(userInputText);
-                    input.setText("");
-                    userInputText = userInputText.toLowerCase();
-                    userInput.add(userInputText);
-                    if (userInputText.contains("6s")){
-                        goToWebpage("http://www.apple.com/shop/buy-iphone/iphone6s");
-                    } else if (userInputText.contains("6")){
-                        goToWebpage("http://www.apple.com/shop/buy-iphone/iphone6");
-                    } else if (userInputText.contains("5s")){
-                        goToWebpage("http://www.apple.com/shop/buy-iphone/iphone5s");
-                    } else {
-                        String done = "I'm sorry, I don't understand";
-                        botOut = (TextView)findViewById(R.id.botOut1);
-                        botOut.setText(done);
-                        buyPhone(i, phone);
-                    }
-                } else if (userInputText.contains("samsung")){
-                    phone.setBrand("samsung");
-                    keywords.add("samsung");
-                    i++;
-                    buyPhone(i, phone);
-                } else if (userInputText.contains("lg")){
-                    phone.setBrand("lg");
-                    keywords.add("lg");
-                    i++;
-                    buyPhone(i, phone);
-                } else if (userInputText.contains("google")){
-                    botOut = (TextView)findViewById(R.id.botOut5);
-                    d = "Which model do you want, 6p or 5x?";
-                    botOut.setText(d);
-                    in = input.getText().toString();
-                    b = "User: ";
-                    userInputText = b + in;
-                    userIn = (TextView)findViewById(R.id.userIn1);
-                    userIn.setText(userInputText);
-                    input.setText("");
-                    userInputText = userInputText.toLowerCase();
-                    userInput.add(userInputText);
-                    if (userInputText.contains("6p")){
-                        goToWebpage("https://store.google.com/product/nexus_6p");
-                    } else if (userInputText.contains("5x")){
-                        goToWebpage("https://store.google.com/product/nexus_5x");
-                    } else {
-                        String done = "I'm sorry, I don't understand";
-                        botOut = (TextView)findViewById(R.id.botOut1);
-                        botOut.setText(done);
-                        buyPhone(i, phone);
-                    }
-                } else {
-                    String done = "I'm sorry, I don't understand";
-                    botOut = (TextView)findViewById(R.id.botOut5);
-                    botOut.setText(done);
-                    buyPhone(i, phone);
-                }
-
-            case 4:
-                botOut = (TextView)findViewById(R.id.botOut5);
-                d = "What screensize would youe like, 5 inch or phablet?";
-                botOut.setText(d);
-                in = input.getText().toString();
-                b = "User: ";
-                userInputText = b + in;
-                userIn = (TextView)findViewById(R.id.userIn1);
-                userIn.setText(userInputText);
-                input.setText("");
-                userInputText.toLowerCase();
-                userInput.add(userInputText);
-                if (userInputText.contains("5")){
-                    phone.setScreenSize(5);
-                    if (phone.getBrand() == "lg"){
-                        phone.setModel("g");
-                    } else if (phone.getBrand() == "samsung"){
-                        phone.setModel("galaxy+s");
-                    }
-                } else if (userInputText.contains("phablet")){
-                    phone.setScreenSize(6);
-                    if (phone.getBrand() == "lg"){
-                        phone.setModel("v10");
-                    } else if (phone.getBrand() == "samsung"){
-                        phone.setModel("note");
-                    }
-                    botOut = (TextView)findViewById(R.id.botOut1);
-                }
-                d = "Which model do you want, 5s, 6 or 6s?";
-                botOut.setText(d);
-                in = input.getText().toString();
-                b = "User: ";
-                userInputText = b + in;
-                userIn = (TextView)findViewById(R.id.userIn1);
-                userIn.setText(userInputText);
-                input.setText("");
-                userInputText.toLowerCase();
-                userInput.add(userInputText);
-        }
-
-    }
-    private void buyLaptop() {
-
-    }
-    public void chat(int i){
+    public void chat(){
         String in = input.getText().toString();
         String b = "User: ";
         String userInputText = b + in;
 
-        switch (i){
+        switch (conversationState) {
             case 0:
                 userIn = (TextView)findViewById(R.id.userIn1);
                 userIn.setText(userInputText);
@@ -254,14 +98,16 @@ public class MainActivity extends AppCompatActivity {
                     botOut.setText(d);
                     botOutput.add(d);
                     conversationState++;
-                } else if (userInputText.contains("good") || userInputText.contains("great")){
+                } else if (userInputText.contains("good") || userInputText.contains("great")||userInputText.contains("dandy")){
                     botOut = (TextView)findViewById(R.id.botOut2);
                     String f;
                     if (userInputText.contains("great")){
                         f = "great";
+                    } else if (userInputText.contains("dandy")){
+                        f = "dandy";
                     } else {
                         f = "good";
-                    } String d = "ShopBot: I'm glad you're feeling  " + f + "!\nWould you like to buy a phone or a laptop?";
+                    } String d = "ShopBot: I'm glad you're feeling " + f + "!\nWould you like to buy a phone or a laptop?";
                     botOut.setText(d);
                     botOutput.add(d);
                     conversationState++;
@@ -277,24 +123,37 @@ public class MainActivity extends AppCompatActivity {
                 input.setText("");
                 userInputText = userInputText.toLowerCase();
                 userInput.add(userInputText);
-                if (userInputText.contains("phone")){
-                    Smartphone phone = new Smartphone();
-                    buyPhone(1, phone);
-                } else if (userInputText.contains("laptop")){
-                    buyLaptop();
-                } else {
-                    String d = "I'm sorry, I don't understand";
-                    botOut = (TextView)findViewById(R.id.botOut3);
-                    botOut.setText(d);
-                }
+                botOut = (TextView)findViewById(R.id.botOut3);
+                String d = "ShopBot: OKay, phone it is! Would you like Android or iPhone?";
+                botOut.setText(d);
+                conversationState++;
                 break;
-
-
+            case 3:
+                userIn = (TextView)findViewById(R.id.userIn4);
+                userIn.setText(userInputText);
+                input.setText("");
+                userInputText = userInputText.toLowerCase();
+                userInput.add(userInputText);
+                botOut = (TextView)findViewById(R.id.botOut4);
+                d = "ShopBot: That's great, you want an iPhone. Would you like a 5s, 6, or 6s?";
+                botOut.setText(d);
+                conversationState++;
+                break;
+            case 4:
+                userIn = (TextView)findViewById(R.id.userIn5);
+                userIn.setText(userInputText);
+                input.setText("");
+                userInputText = userInputText.toLowerCase();
+                userInput.add(userInputText);
+                botOut = (TextView)findViewById(R.id.botOut5);
+                d = "ShopBot: Give me a second to find your result.";
+                botOut.setText(d);
+                goToWebpage("http://www.apple.com/shop/buy-iphone/iphone6s");
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         input = (EditText)findViewById(R.id.input);
@@ -316,7 +175,7 @@ public class MainActivity extends AppCompatActivity {
         Button enter = (Button)findViewById(R.id.enter);
         enter.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
-                chat(conversationState);
+                chat();
             }
         });
     }
